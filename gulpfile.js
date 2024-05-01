ad9c1b67e9cf6a144292ed00506699dff09034fa
Jonnathan23 <jsaquicela26@gmail.com>
2024-05-01 18:37:45 -0500

    Implementacion de componentes sass y actualización automatica

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+import {src, dest, watch}from 'gulp'
+import gulpSass from 'gulp-sass'
+import * as dartSass from 'sass'
+
+const sass = gulpSass(dartSass)
+
+export function css( done){
+    src('src/scss/app.scss')
+    .pipe( sass() )
+    .pipe( dest('build/css') )
+
+    done()
+}
+
+export function dev(){
+    watch('src/scss/app.scss', css)
+}
